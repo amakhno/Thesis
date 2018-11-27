ed27856854743ba72d99e2e3261e994757e6e858
Noir <artem_mahno@mail.ru>
2018-11-27 12:43:51 +0300

    Made right settings (for linter)

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import scipy
+from scipy import special
+import numpy as np
 
-a = 10
-print a
+#Function
+def ReadVariable (input_file, var_name):
+    line = input_file.readline()
+    if var_name != line[0: len(var_name)]:
+        return 0
+    value = line[len(var_name) + 3:]
+    return float(value)
+
+input_file = open("input.data", "r") 
+tempValue = ReadVariable(input_file, "tempValue")
+item = special.hyp1f1(1, tempValue, 1)
+print (item)
