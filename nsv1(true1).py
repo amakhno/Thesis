c13ae6407a22a96ad503e0e0469117d2e0c770a3
Artem Makhno <artem_mahno@mail.ru>
2019-02-02 01:18:46 +0300

    results

--- nsv1(true1).py
+++ nsv1(true1).py
@@ -42,6 +42,7 @@ def lf(ef):
 def li(ei):
     return z1*z2*aa*math.sqrt(m/2/ei)
 
+
 def ki(ei):
     return math.sqrt(2*m*ei)
 
@@ -59,10 +60,11 @@ def fun1(t):
     c = 1
     res = mp.hyp2f1(a, b, c, x)
     checkRes = (1-x)**(c-a-b) * mp.hyp2f1(c-a, c-b, c, x)
-    diff = np.sqrt((res.imag - checkRes.imag)**2 + (res.real - checkRes.real)**2)
-    if (diff > 1e-10):
-        print("Error: " + diff)
-    return 1e-300*(res.real*res.real+res.imag*res.imag)/(ki*ki+kf*kf-2*ki*kf*t)/(ki*ki+kf*kf-2*ki*kf*t)
+    diff = np.sqrt((res.imag - checkRes.imag)**2 +
+                   (res.real - checkRes.real)**2)
+    if (diff > 1e-9):
+        print("Error: " + str(diff))
+    return (res.real*res.real+res.imag*res.imag)/(ki*ki+kf*kf-2*ki*kf*t)/(ki*ki+kf*kf-2*ki*kf*t)
 
 
 def sig1(ef):
@@ -77,8 +79,10 @@ def fun2(ef):
     lf = (z1+1)*z2*aa*np.sqrt(m/2/ef)
     ki = np.sqrt(2*m*ei)
     e = ei-d-df-1
-    c = 1e300*np.sqrt(e-ef)*(e-ef)*(e-ef)*(e-ef)/(ki*ki-kf*kf)/(ki*ki-kf*kf)
-    return c/(np.exp(2*np.pi*lf)-1)*sig1(ef)
+    c = np.sqrt(e-ef)*(e-ef)*(e-ef)*(e-ef)/(ki*ki-kf*kf)/(ki*ki-kf*kf)
+    sig1res = sig1(ef)
+    return c/(np.exp(2*np.pi*lf)-1)*sig1res
+
 
 def fun3(ei):
     global send_ei
@@ -86,20 +90,37 @@ def fun3(ei):
     li = z1*z2*aa*np.sqrt(m/2/ei)
     res = integrate.quad(lambda x: fun2(x), 0.0, ei-d-df-1)[0]
     ww = ei*np.exp(-ei/tt)
-    return ww*z2*z2*256*np.sqrt(2)*aa*aa*aa*aa*gv*gv*m*m*m*m*m*z1*(z1+1)* z2*z2/(105*math.pi*ei)/(1-np.exp(-2*math.pi*li))*res
+    return ww*z2*z2*256*np.sqrt(2)*aa*aa*aa*aa*gv*gv*m*m*m*m*m*z1*(z1+1) * z2*z2/(105*math.pi*ei)/(1-np.exp(-2*math.pi*li))*res
 
 
 def nsv(d):
     res = integrate.quad(lambda x: fun3(x), d+df+1, np.inf)
     return math.sqrt(8/math.pi/m/tt/tt/tt)*res[0]*0.19448
 
+def nsv_norm(d):
+    return nsv(d)*44.722E-12
 
-#print(nsv(d)*44.722E-12)
-if __name__ == "__main__":
-    vectorisedFunc3 = np.vectorize(fun3)
+t_array = np.linspace(1e8, 1e10, 24)
+#vectorisedFunc3 = np.vectorize(nsv_norm)
+#t_result = np.linspace(1e8, 1e10, 24)
+tt = 1e8
+print(nsv_norm(d))
+f1 = open('./nsv1(true1).out', 'w')
+#for i in range(0, len(t_array)):
+    #tt = t_array[i]
+    #res = nsv_norm(d)
+    #print(str(t_array[i]) + ' ' + str(res), file=f1)
 
-    x = np.linspace(d+df+1, 100, 10)
-    p = multiprocessing.Pool()
-    mp_solutions = p.map(fun3, x)
+# print(nsv(d)*44.722E-12)
+# if __name__ == "__main__":
+#     vectorisedFunc3 = np.vectorize(fun3)
 
-    pylab.plot(x,mp_solutions)
+#     xz = np.linspace(d+df+1+0.2, 10, 5)
+
+#     yz = vectorisedFunc3(xz)
+#     #p = multiprocessing.Pool(11)
+#     #mp_solutions = p.map(fun3, x)
+
+#     pylab.autoscale(enable=True, axis='both', tight=True)
+#     pylab.plot(xz,yz)
+#     pylab.show()
