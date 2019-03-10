bccb3af729f449dd7084396a7ee456a04773c52e
Artem Makhno <artem_mahno@mail.ru>
2019-03-10 07:55:42 -0700

    solution fix

--- solve.py
+++ solve.py
@@ -1,11 +1,12 @@
 import numpy as np
+import pylab
 
 with open('out-fuc-3-true.txt') as f:
     content = f.readlines()
 
 content = [x.strip() for x in content]
 
-t_array = np.linspace(1e8, 1e10, 24)
+t_array = np.linspace(0.1, 10, 24)
 sig_array = []
 
 for index in range(0, len(content)):
@@ -17,7 +18,6 @@ for index in range(0, len(content)):
     else:
         sig_array.append(np.log(np.abs(val)))
     
-
 
 def build_a(t_arr):
     if isinstance(t_arr, np.ndarray):
@@ -34,8 +34,45 @@ def build_a(t_arr):
 a = build_a(t_array)
 b = sig_array
 f=open('solved.txt', 'w')
-print(np.linalg.solve(a[0:7], b[0:7]), file=f)
 
-x, _, _, _ =np.linalg.lstsq(a, b, rcond=1)
-print(x, file=f)
-f.close()
+x = []; y = []
+
+for index in range(0, 24-7):
+    x.append(t_array[index])
+    y.append(np.linalg.solve(a[index:index + 7], b[index:index + 7]))
+
+def plot_solution(t_array, solution, block):
+    if (block != True):
+        block = False
+    a1_array = []; b1_array = []
+    for t_index in range(0, len(t_array)):
+        current_t = t_array[t_index]
+        current_a1 = 0
+        for a_index in range(0, len(solution)):
+            current_a1 += solution[a_index] * a[t_index][a_index]
+        a1_array.append(current_a1)
+        b1_array.append(b[t_index])
+    pylab.figure()
+    line1, = pylab.plot(t_array, a1_array, label="A", linestyle="--")
+    line2, = pylab.plot(t_array, b1_array, label="B")    
+    pylab.legend(handles=[line1, line2], loc=1)
+    pylab.show(block=block)
+
+print("lstsq1")
+x_lstsq, _, _, _ =np.linalg.lstsq(a, b, rcond=1)
+plot_solution(t_array, x_lstsq, True)
+print("lstsq2")
+x_lstsq, _, _, _ =np.linalg.lstsq(a[2:20], b[2:20], rcond=1)
+plot_solution(t_array, x_lstsq, True)
+
+for sol_index in range(0, len(y)):
+    current_sol = y[sol_index]
+    plot_solution(t_array, current_sol, True)
+
+#pylab.plot(x, y)
+#pylab.xticks(x, x, rotation='vertical', )
+#pylab.show()
+
+# print(np.linalg.solve(a[0:7], b[0:7]), file=f)
+
+# f.close()
