2899cc75a606ff0e35c558eee2a9ad0b16907221
Artem Makhno <artem_mahno@mail.ru>
2019-03-25 00:45:26 -0700

    Write good formatter

--- solve.py
+++ solve.py
@@ -12,7 +12,7 @@ sig_array = []
 for index in range(0, len(content)):
     splitted = content[index].rsplit(' ')
     t_array.append(float(splitted[0])/1e9)
-    val = float(splitted[1]) * 1e25
+    val = float(splitted[1]) * 1e50
     if (val > 0):
         sig_array.append(np.log(val))
     else:
@@ -47,7 +47,6 @@ def plot_solution(t_array, solution, block):
         block = False
     a1_array = []; b1_array = []
     for t_index in range(0, len(t_array)):
-        current_t = t_array[t_index]
         current_a1 = 0
         for a_index in range(0, len(solution)):
             current_a1 += solution[a_index] * a[t_index][a_index]
@@ -59,13 +58,29 @@ def plot_solution(t_array, solution, block):
     pylab.legend(handles=[line1, line2], loc=1)
     pylab.show(block=block)
 
-print("lstsq1")
-x_lstsq, _, _, _ =np.linalg.lstsq(a, b, rcond=1)
-plot_solution(t_array, x_lstsq, True)
-print("lstsq2")
-x_lstsq, _, _, _ =np.linalg.lstsq(a[2:20], b[2:20], rcond=1)
-plot_solution(t_array, x_lstsq, True)
-print(x_lstsq, file=f)
+x_lstsq_full, _, _, _ =np.linalg.lstsq(a, b, rcond=1)
+# plot_solution(t_array, x_lstsq, False)
+# -6.461089e+02-2.855431e+02 4.604533e+03-3.632646e+03                      
+#  1.326413e+02-5.591785e+00 2.405325e+03     
+# plot_solution(t_array, [-6.461089e2,-2.855431e2,4.604533e3,-3.632646e3,1.326413e2, -5.591785, 2.405325e3], True)
+# x_lstsq_part, _, _, _ =np.linalg.lstsq(a[2:20], b[2:20], rcond=1)
+# plot_solution(t_array, x_lstsq, True)
+# print(x_lstsq_full, file=f)
+
+def save_to_file(x_lstsq_full, filename, elem1, elem2):
+    save_file = open(filename, 'w')
+    print("1", file = save_file)
+    print("     {:>5}{:>5}                            cleaw     0.00000e+00          ".format(elem1, elem2), file = save_file)
+    prettyfied = []
+    for index in range(len(x_lstsq_full)):
+        prettyfied.append("{0:1.6e}".format(x_lstsq_full[index]))
+    print("{:>13}{:>13}{:>13}{:>13}                      ".format(prettyfied[0], prettyfied[1],prettyfied[2],prettyfied[3]), file = save_file)
+    print("{:>13}{:>13}{:>13}                                   ".format(prettyfied[4], prettyfied[5],prettyfied[6]), file = save_file)
+    save_file.close()
+
+save_to_file(x_lstsq_full, "mylib.txt", "ag108", "cd108")
+print(x_lstsq_full)
+
 f.close()
 
 #pylab.plot(x, y)
